a4907afb5fbbb3c05b83166692f3275eb5111fa9
Nakul Gupta <114728605+nakul2207@users.noreply.github.com>
2025-05-21 16:26:13 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import Mainrouter from './routes/main';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000', 10);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
